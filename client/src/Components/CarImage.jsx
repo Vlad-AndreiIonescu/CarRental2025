5014739103901c6a2a0e631802f05cd2ed7a2a67
Vlad-AndreiIonescu <75218385+Vlad-AndreiIonescu@users.noreply.github.com>
2025-07-07 23:22:28 +0300

    ar trb sa mearga si deplyed acum10

--- client/src/Components/CarImage.jsx
+++ client/src/Components/CarImage.jsx
@@ -1,10 +1,14 @@
 const CarImage = ({ image, make, model }) => {
+  const handleImageError = (e) => {
+    e.target.src = "/default-car.png";
+  };
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden max-h-[450px]">
       <img
         src={image || "/default-car.png"}
         alt={`${make} ${model}`}
         className="w-full h-[400px] object-cover object-center"
+        onError={handleImageError}
       />
     </div>
   );
